6dfad9fff1f6d751cafc1b06592ccf8db75c8b32
Emanon <EmanonSS100@gmail.com>
2025-02-14 09:10:36 +0800

    店鋪營業狀態代碼實現

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,47 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Api(tags = "店鋪操作接口")
+@Slf4j
+public class ShopController {
+    //店铺营业状态常量
+    public static final String  KEY = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 设置营业状态
+     * @param status
+     * @return
+     */
+    @PutMapping("/{status}")
+    @ApiOperation("设置营业状态")
+    public Result setStatus(@PathVariable Integer status){
+        log.info("设置营业状态:{}",status == 1 ? "营业中" : "打样中");
+        redisTemplate.opsForValue().set(KEY,status);
+        return Result.success();
+    }
+
+    /**
+     * 查询店铺营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("查询店铺营业状态")
+    public Result<Integer> getStatus(){
+        log.info("查询店铺营业状态");
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("设置营业状态:{}",status == 1 ? "营业中" : "打样中");
+        return Result.success(status);
+    }
+}
